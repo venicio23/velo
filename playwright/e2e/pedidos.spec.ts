ddf437e349be335318bceeba252ce041496be10a
Venicio Viana <venicviana@gmail.com>
2026-03-14 00:19:40 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,22 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('Deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/');
-
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-GIU6SK');
   await page.getByTestId('search-order-button').click();
-  
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-GIU6SK');
-  
+
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
