6c6403e0c306dacdac7584c3e192f65f2346ffdf
Venicio Viana <venicviana@gmail.com>
2026-06-21 14:25:49 -0300

    Refactor: Validações de Interface Além do Tradicional com Snapshots

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -21,13 +21,43 @@ test.describe('Consultar Pedido', () => {
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
     //Assert
-    const containerPedido = page.getByRole('paragraph')
+/*     const containerPedido = page.getByRole('paragraph')
       .filter({ hasText: /^Pedido$/ })
       .locator('..'); //Sobe para o elemento pai do texto "Pedido"
 
     await expect(containerPedido).toContainText(orderCode, { timeout: 10000 });
 
-    await expect(page.getByText('APROVADO')).toBeVisible();
+    await expect(page.getByText('APROVADO')).toBeVisible(); */
+
+
+    await expect(page.getByTestId(`order-result-${orderCode}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${orderCode}
+      - img
+      - text: APROVADO
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: Lunar White
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: aero Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: Marcos Viana
+      - paragraph: Email
+      - paragraph: veniqa@gmail.com
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: À Vista
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
 
   });
 
