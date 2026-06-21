2c0aa00050ea7cf7f4e2ac1cff247908904883c4
Venicio Viana <venicviana@gmail.com>
2026-06-21 15:36:18 -0300

    Refactor: Um Fluxo, Dois Resultados

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -14,10 +14,21 @@ test.describe('Consultar Pedido', () => {
 
   test('Deve consultar um pedido aprovado', async ({ page }) => {
     //Test Data
-    const orderCode = 'VLO-GIU6SK';
+    //const orderCode = 'VLO-GIU6SK';
+    const order = {
+      number: 'VLO-GIU6SK',
+      status: 'APROVADO',
+      color: 'Lunar White',
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'Marcos Viana',
+        email: 'veniqa@gmail.com'
+      },
+      payment: 'À Vista'
+    };
 
     //Act
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(orderCode);
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
     //Assert
@@ -30,32 +41,92 @@ test.describe('Consultar Pedido', () => {
     await expect(page.getByText('APROVADO')).toBeVisible(); */
 
 
-    await expect(page.getByTestId(`order-result-${orderCode}`)).toMatchAriaSnapshot(`
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
       - img
       - paragraph: Pedido
-      - paragraph: ${orderCode}
+      - paragraph: ${order.number}
       - img
       - text: APROVADO
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
       - paragraph: Cor
-      - paragraph: Lunar White
+      - paragraph: ${order.color}
       - paragraph: Interior
       - paragraph: cream
       - paragraph: Rodas
-      - paragraph: aero Wheels
+      - paragraph: ${order.wheels}
       - heading "Dados do Cliente" [level=4]
       - paragraph: Nome
-      - paragraph: Marcos Viana
+      - paragraph: ${order.customer.name}
       - paragraph: Email
-      - paragraph: veniqa@gmail.com
+      - paragraph: ${order.customer.email}
       - paragraph: Loja de Retirada
       - paragraph
       - paragraph: Data do Pedido
       - paragraph: /\\d+\\/\\d+\\/\\d+/
       - heading "Pagamento" [level=4]
-      - paragraph: À Vista
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+  });
+
+  test('Deve consultar um pedido reprovado', async ({ page }) => {
+    //Test Data
+    //const orderCode = 'VLO-GNNR8X';
+    const order = {
+      number: 'VLO-GNNR8X',
+      status: 'REPROVADO',
+      color: 'Midnight Black',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Steve Jobs',
+        email: 'jobs@apple.com'
+      },
+      payment: 'À Vista'
+    };
+
+    //Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    //Assert
+/*     const containerPedido = page.getByRole('paragraph')
+      .filter({ hasText: /^Pedido$/ })
+      .locator('..'); //Sobe para o elemento pai do texto "Pedido"
+
+    await expect(containerPedido).toContainText(orderCode, { timeout: 10000 });
+
+    await expect(page.getByText('APROVADO')).toBeVisible(); */
+
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: REPROVADO
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
       `);
 
